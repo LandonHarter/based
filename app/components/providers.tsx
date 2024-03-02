3b9aef79a5b80d62d9d5f051a445e84e77efeef7
LandonHarter <landonharter@outlook.com>
2024-03-01 23:28:21 -0500

    initial

---
+++ app/components/providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { NextUIProvider } from "@nextui-org/react";
+import { Toaster } from "sonner";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <NextUIProvider>
+            <Toaster richColors />
+            {children}
+        </NextUIProvider>
+    );
+}
