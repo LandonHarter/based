49fa3c60c95647c39d4973841b29318447d59cfe
LandonHarter <landonharter@outlook.com>
2024-03-02 21:01:37 -0500

    fix: return the highest quality image

--- app/backend/ai/image.ts
+++ app/backend/ai/image.ts
@@ -7,5 +7,13 @@ export async function generateImage(title: string) {
     const images = await client.search(title, {
         safe: "high"
     });
-    return images[0].url;
+
+    let maxRes = { resX: 0, index: -1 };
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        if (image.width > maxRes.resX) maxRes = { resX: image.width, index: i };
+    }
+
+    if (maxRes.index === -1) return images[0].url;
+    return images[maxRes.index].url;
 }
