77431cd0020e07d2cf48367f05672f25a9371fbb
LandonHarter <landonharter@outlook.com>
2024-03-03 20:10:51 -0500

    fix: forgot to remove export

--- app/(pages)/analyze/page.tsx
+++ app/(pages)/analyze/page.tsx
@@ -1,10 +1,5 @@
-import { analyzeSources } from "@/backend/ai/analyze";
-import { parseResponse } from "@/backend/ai/parse";
-import { Button } from "@nextui-org/react";
-import * as admin from 'firebase-admin';
 import AnalyzeForm from "./analyze";
 
-export const maxDuration = 30;
 export const metadata = {
     title: 'Analyze an Event | Based',
     description: 'Contribute to the Based community by submitting multiple news sources on the same event to help clear up bias and misinformation.'
