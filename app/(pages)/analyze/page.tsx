cccf68557bacae85184384d543f156757af954aa
LandonHarter <landonharter@outlook.com>
2024-03-03 19:19:26 -0500

    fix: unallowed export

--- app/(pages)/analyze/page.tsx
+++ app/(pages)/analyze/page.tsx
@@ -4,10 +4,12 @@ import { Button } from "@nextui-org/react";
 import * as admin from 'firebase-admin';
 import AnalyzeForm from "./analyze";
 
+export const maxDuration = 30;
 export const metadata = {
     title: 'Analyze an Event | Based',
     description: 'Contribute to the Based community by submitting multiple news sources on the same event to help clear up bias and misinformation.'
 };
+
 export default function AnalyzePage() {
     return (
         <main className="w-screen flex flex-col items-center">
