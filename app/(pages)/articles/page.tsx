586e5bf226d81a6cb3c739679e35032f56f69059
LandonHarter <landonharter@outlook.com>
2024-03-03 20:19:06 -0500

    fix: /articles page wasn't being server rendered

--- app/(pages)/articles/page.tsx
+++ app/(pages)/articles/page.tsx
@@ -1,7 +1,7 @@
 import { getArticles } from "@/backend/db/articles";
-import Image from "next/image";
 import Link from "next/link";
 
+export const revalidate = 0;
 export default async function ArticlesPage() {
     const articles = await getArticles();
 
